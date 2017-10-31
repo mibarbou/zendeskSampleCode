33524ba5fe9b03a06a21aa7ba95b6d152e2a7d80
Michel Barbou Salvador <mibarbou@gmail.com>
2017-10-31 18:32:17 +0100

    - Adds initial xcode empty project with ArticlesTableViewController. - Adds .gitignore file.

---
+++ zendeskGuide/zendeskGuide/ArticleCell.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleCell.swift
+//  zendeskGuide
+//
+//  Created by Michel Barbou Salvador on 31/10/2017.
+//  Copyright © 2017 mibarbou. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+    static let identifier = "ArticleCell"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
